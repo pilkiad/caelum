cd1a02e847be36bce8cbe22e2fe637863d4e98dd
pilkiad <pilkiad+gitkraken@proton.me>
2024-10-26 12:36:15 +0200

    #2 -change: Made environment respect scope
    Note that append_or_update does not search within a higher scope since altering outside of your scope is illegal

--- funl/utils/environment.py
+++ funl/utils/environment.py
@@ -3,24 +3,34 @@
 from . import globalvars
 
 
-def get_function_from_name(name: str) -> FunctionDefinition | None:
-    for environment_function in globalvars.environment:
+def get_function_from_name(name: str, index: int = -1) -> FunctionDefinition | None:
+    for environment_function in globalvars.environment[index]:
         if environment_function.name == name:
             return environment_function
-    return None
+
+    if index != len(globalvars.environment) * -1:
+        return get_function_from_name(name, index=(index-1))
+    else:
+        return None
 
 
-def get_function_index_from_name(name: str) -> int | None:
-    for i in range(0, len(globalvars.environment)):
+def get_function_index_from_name(name: str, index: int = -1) -> int | None:
+    for i in range(0, len(globalvars.environment[index])):
         if globalvars.environment[i].name == name:
             return i
-    return None
+
+    if index != len(globalvars.environment) * -1:
+        return get_function_from_name(name, index=(index-1))
+    else:
+        return None
 
 
 def append_or_update(function_definition: FunctionDefinition) -> None:
+    # NOTE - no possibility to change the index here since writing should only
+    # be possible to the current scope
     result = get_function_index_from_name(function_definition.name)
 
     if result is not None:
-        globalvars.environment[result] = function_definition
+        globalvars.environment[-1][result] = function_definition
     else:
-        globalvars.environment.append(function_definition)
+        globalvars.environment[-1].append(function_definition)
