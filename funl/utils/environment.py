755d114ad66d83157d3821c6083d363d5150df09
pilkiad <pilkiad+gitkraken@proton.me>
2025-01-16 11:57:07 +0100

    (change) Polished environment

--- funl/utils/environment.py
+++ funl/utils/environment.py
@@ -1,4 +1,10 @@
-# TODO - add module comment
+"""
+> Manages the environments in which funl functions operate
+
+This class provides an interface to create or find functions in the current environment
+aswell as create and pop environments.
+This is currently used by the interpreter when calling function or returning from them.
+"""
 
 from . import logger
 from . import globalvars
@@ -6,74 +12,93 @@ from .function_definition import FunctionDefinition
 
 
 def get_function_from_name(name: str, index: int = -1) -> FunctionDefinition | None:
+    """
+    Returns FunctionDefinition searched by a given name
+
+    name: str   The name to search for
+    index: int  The starting level of environment hirarchy, will begin locally
+                and work its way up to global env
+
+    FunctionDefinition if the function was found,
+    None otherwise
+    """
+
     for environment_function in globalvars.environment[index]:
         if environment_function.name == name:
             return environment_function
 
+    # If we did not find the function, try an upper level environment
     if index != len(globalvars.environment) * -1:
         return get_function_from_name(name, index=(index-1))
     else:
         return None
 
 
-def get_function_index_from_name(name: str, index: int = -1) -> int | None:
+def get_local_function_index_from_name(name: str) -> int | None:
     """
     Fetches a custom function index inside of its environment
     Will default to the current environment but an environment index can be specified
 
     name: str   The name of the function
-    index: int  The index of the environment, default: -1 to select the current
-                local environment
+    
+    NOTE - The function index (level of environment) can only ever be -1 as this
+    function is usually just called by append_or_update() which may only happen
+    at the current level of environment depth
 
-    The index of the function in its environment if found,
+    Returns the index of the function in its environment if found,
     None otherwise
     """
 
     # Search in the environment with the specified index
-    for i in range(0, len(globalvars.environment[index])):
+    for i in range(0, len(globalvars.environment[-1])):
         # Go through all entries in the environment
-        if globalvars.environment[index][i].name == name:
+        if globalvars.environment[-1][i].name == name:
             return i
 
-    # FIXME - i do not know why i wrote this or what it does
-    #if index != len(globalvars.environment) * -1:
-    #    logger.log_error("environment", "what")
-    #    return get_function_from_name(name, index=(index-1))
-    #else:
     return None
 
 
 def append_or_update(function_definition: FunctionDefinition) -> None:
-    # NOTE - no possibility to change the index here since writing should only
-    # be possible to the current scope
-    result = get_function_index_from_name(function_definition.name)
+    """
+    Will append or update the function given to the current environment
+
+    function_definition: FunctionDefinition The function to be updated or appended
+                                            if it does not already exist
+    """
+
+    result = get_local_function_index_from_name(function_definition.name)
 
     if result is not None:
-        logger.log_debug("environment", f"append_or_update => update {function_definition.name}")
+        logger.log_debug("Environment", f"append_or_update => update {function_definition.name}")
         globalvars.environment[-1][result] = function_definition
     else:
-        logger.log_debug("environment", f"append_or_update => append {function_definition.name}")
+        logger.log_debug("Environment", f"append_or_update => append {function_definition.name}")
         globalvars.environment[-1].append(function_definition)
 
 
 def new() -> None:
     """
-    Creates a new level of the environment.
-    Typically called when a custom function was called in the interpreter.
+    Creates a new level of the environment
+    Typically called when a custom function was called in the interpreter
     """
 
-    logger.log_debug("environment", f"new (depth: {len(globalvars.environment)+1})")
+    logger.log_debug("Environment", f"new (depth: {len(globalvars.environment)+1})")
 
     globalvars.environment.append([])
 
 
 def pop() -> None:
     """
-    Removes the current layer of environment.
-    Typically called when a custom function has returned in the interpreter.
-    """
-    # TODO - Does return() call this?
+    Removes the current layer of environment
+    Typically called when a custom function has returned in the interpreter
 
-    logger.log_debug("environment", f"pop (depth: {len(globalvars.environment)-1})")
+    Errors on trying to pop global environment
+    """
+    
+    logger.log_debug("Environment", f"pop (depth: {len(globalvars.environment)-1})")
+
+    if len(globalvars.environment) == 1:
+        logger.log_info("Environment", "Did you call 'return()' outside of a function?")
+        logger.log_error("Environment", "Tried to pop the global environment")
 
     globalvars.environment.pop()
